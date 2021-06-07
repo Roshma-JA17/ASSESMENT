e03ea1cca63e4e175cea743881c21a9eceb261b4
Roshma-JA17 <84120858+Roshma-JA17@users.noreply.github.com>
2021-06-07 11:16:28 +0530

    Add files via upload

---
+++ contactmanagerappusingcollection/src/main/java/com/capgemini/cmapp/App.java
@@ -0,0 +1,10 @@
+package com.capgemini.cmapp;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ contactmanagerappusingcollection/src/test/java/com/capgemini/cmapp/AppTest.java
@@ -0,0 +1,17 @@
+package com.capgemini.cmapp;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class AppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(false);
+	}
+	
+
+}
